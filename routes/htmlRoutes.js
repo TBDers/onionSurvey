47ebe1ce47cd0ffbf205f46f3c9ade75a1397782
jterry63 <jordankterry@outlook.com>
2018-08-07 20:53:44 -0600

    added-initial-routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,24 @@
 var db = require("../models");
 var path = require('path')
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/index.html"));
   });
+
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/survey.html"));
+  });
+
+  app.get("/register", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/register.html"));
+  });
+
+  app.get("/data", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/data.html"));
+  });
+
+
 
   // // Load example page and pass in an example by id
   // app.get("/example/:id", function(req, res) {
@@ -12,7 +26,7 @@ module.exports = function(app) {
   // });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
--- server.js
+++ server.js
@@ -8,10 +8,11 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+
 
 // Handlebars
 
@@ -30,13 +31,13 @@ if (process.env.NODE_ENV === "test") {
 
 // Starting the server, syncing our models ------------------------------------/
 // db.sequelize.sync(syncOptions).then(function () {
-  app.listen(PORT, function () {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+app.listen(PORT, function () {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
+});
 // });
 
 module.exports = app;

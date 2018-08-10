926772ce1d576121421414d4d8c7422e4aeb894d
jterry63 <jordankterry@outlook.com>
2018-08-10 00:19:29 -0600

    jaws-db

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,20 @@ var mysql = require("mysql");
 //     dialect: "mysql"
 // host: "mnwax9mxzow0k7hi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// var connection = mysql.createConnection({
+//     host: "localhost",
 
+// Your port; if not 3306
+// port: 3306,
+
+// Your username
+// user: "root",
+
+// Your password
+// password: "",
+// database: "onion_db"
+// });
+
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 module.exports = connection;

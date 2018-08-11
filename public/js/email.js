b978ca986d3d0478fde502be77a894e2db1b4bc9
Al Oshiro <35230982+ahoshiro@users.noreply.github.com>
2018-08-11 08:51:06 -0600

    Email add

---
+++ public/js/email.js
@@ -0,0 +1,55 @@
+function emailfunction () {
+    var emailNames = str.valueOf();
+    document.getElementById("email").innerHTML = emailNames;
+    
+    console.log (emailNames);
+
+    if (emailNames === ""){
+        alert ("Please insert email addresses. Eg: onion1@onionsurvey.com , onion2@onionsurvery.com, etc.");
+    }
+    
+    else {
+        var template_params = {
+            "to_name": "Cepa Head",
+            "from_name": "Onion Survey",
+            "message_html": "Go to www.Onionsurvey.com if you're awesome and take the survey!",
+            "user_email": emailNames
+        }
+        
+        var service_id = "default_service";
+        var template_id = "template_fI64wY8s";
+        emailjs.send(service_id,template_id,template_params);
+
+        return false;};
+};
+
+
+
+
+
+
+
+// var myform = $("form#myform");
+// myform.submit(function(event){
+// 	event.preventDefault();
+
+// 	var params = myform.serializeArray().reduce(function(obj, item) {
+//      obj[item.name] = item.value;
+//      return obj;
+//   }, {});
+
+//   // Change to your service ID, or keep using the default service
+//   var service_id = "default_service";
+
+//   var template_id = "template_fI64wY8s";
+//   myform.find("button").text("Sending...");
+//   emailjs.send(service_id,template_id,params)
+//   	.then(function(){ 
+//        alert("Sent!");
+//        myform.find("button").text("Send");
+//      }, function(err) {
+//        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+//        myform.find("button").text("Send");
+//     });
+//   return false;
+// });

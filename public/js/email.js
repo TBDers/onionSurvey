2977744bd2ddf7d371d7cdf30dc25f5cc5d260c6
jterry63 <jordankterry@outlook.com>
2018-08-11 10:16:34 -0600

    email-js-added

--- public/js/email.js
+++ public/js/email.js
@@ -1,13 +1,17 @@
-function emailfunction () {
-    var emailNames = str.valueOf();
-    document.getElementById("email").innerHTML = emailNames;
-    
-    console.log (emailNames);
+(function () {
+    emailjs.init("user_tj1OIC1yuctFWQRomFWM9");
+})();
 
-    if (emailNames === ""){
-        alert ("Please insert email addresses. Eg: onion1@onionsurvey.com , onion2@onionsurvery.com, etc.");
+$("#sendSurveyButton").on("click", function (event) {
+    event.preventDefault();
+    var emailNames = $("#email").val();
+    // var emailNames = str.valueOf();
+    console.log(emailNames);
+
+    if (emailNames === "") {
+        alert("Please insert email addresses. Eg: onion1@onionsurvey.com , onion2@onionsurvery.com, etc.");
     }
-    
+
     else {
         var template_params = {
             "to_name": "Cepa Head",
@@ -15,13 +19,14 @@ function emailfunction () {
             "message_html": "Go to www.Onionsurvey.com if you're awesome and take the survey!",
             "user_email": emailNames
         }
-        
+
         var service_id = "default_service";
         var template_id = "template_fI64wY8s";
-        emailjs.send(service_id,template_id,template_params);
+        emailjs.send(service_id, template_id, template_params);
 
-        return false;};
-};
+        return false;
+    };
+});
 
 
 
